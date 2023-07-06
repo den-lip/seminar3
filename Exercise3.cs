266aed642a6e4d4dd71a571c231fb292d5dc3c1a
Anastasia <denisovaanastasia202@gmail.com>
2023-07-06 15:12:55 +0300

    Выволнили задание 3

---
+++ Exercise3.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int ReadInt(string message)
+{
+    System.Console.Write(message);              // Подсказка
+    return Convert.ToInt32(Console.ReadLine()); // Преобразование введенной пользователем строки в число
+}
+int N = ReadInt("Введите число => ");
+int count = 1;
+while (count <= N)
+{
+    System.Console.WriteLine(count*count);
+    count++;
+}
